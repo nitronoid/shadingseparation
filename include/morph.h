7ffa225efba3a875a8c04cff5c9dafb0d6106d94
nitronoid <jackdiver@hotmail.co.uk>
2018-11-25 17:21:32 +0000

    Implemented erosion.

---
+++ include/morph.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDED_MORPH_H
+#define INCLUDED_MORPH_H
+
+#include "types.h"
+#include <vector>
+#include <numeric>
+
+BEGIN_AUTOTEXGEN_NAMESPACE
+
+void erode(fpreal* _in, fpreal* o_out, uinteger2 _imageDim, uinteger2 _structuringElement, uinteger _iter);
+
+END_AUTOTEXGEN_NAMESPACE
+
+
+#endif//INCLUDED_MORPH_H
